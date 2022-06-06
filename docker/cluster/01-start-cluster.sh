2dc37af92c1a2c61786d9cd521035e02b15833be
theprotos <vlcz@seznam.cz>
2022-06-06 14:16:13 +0200

    lab-jenkins: new job-dsl

--- docker/cluster/01-start-cluster.sh
+++ docker/cluster/01-start-cluster.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
-echo -e "\n  [$(date +'%Y-%m-%dT%H:%M:%S%z')]: Clean containers"
+printf "\n  [$(date +'%Y-%m-%dT%H:%M:%S%z')]: Clean containers"
 docker-compose rm -s -f -v
 
-echo -e "\n  [$(date +'%Y-%m-%dT%H:%M:%S%z')]: Build agents image"
+printf "\n  [$(date +'%Y-%m-%dT%H:%M:%S%z')]: Build agents image"
 docker-compose -f docker-compose-agents.yml build
 
-echo -e "\n  [$(date +'%Y-%m-%dT%H:%M:%S%z')]: Start jenkins and Docker registry"
+printf "\n  [$(date +'%Y-%m-%dT%H:%M:%S%z')]: Start jenkins and Docker registry"
 docker-compose \
     -f docker-compose.yml \
     up -d \
@@ -14,5 +14,5 @@ docker-compose \
     --build \
     --force-recreate
 
-echo -e "\n  [$(date +'%Y-%m-%dT%H:%M:%S%z')]: Push agents image to docker registry"
+printf "\n  [$(date +'%Y-%m-%dT%H:%M:%S%z')]: Push agents image to docker registry"
 docker-compose -f docker-compose-agents.yml push
